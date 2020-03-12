b76eb40c877dbc64e3a1b376f01c5303639c1d0a
Czl Lebleu <czl.lebleu@gmail.com>
2020-03-11 23:55:26 -0600

    Simplify sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,30 +1,85 @@
 import React from "react"
+import { Link } from "gatsby"
 
-import ThinSidebar from "./sidebar/thin_sidebar.js"
-import AboutSidebar from "./sidebar/about_sidebar.js"
-import style from "./sidebar/sidebar.module.css"
+import styles from "./sidebar.module.scss"
 
-// let AboutSidebarToggle = false;
-// console.log(AboutSidebarToggle)
+import IconContainer from "../images/icons/icon_container"
+import DevToLogo from "../images/icons/devto_logo"
+import DribbbleLogo from "../images/icons/dribbble_logo"
+import GithubLogo from "../images/icons/github_logo"
+import PinterestLogo from "../images/icons/pinterest_logo"
+import TwitterLogo from "../images/icons/twitter_logo"
+import MailLogo from "../images/icons/mail_logo"
+// import ColorModeSwitch from "./sidebar/color_mode"
+import Fleuron from "../images/icons/fleuron"
 
-// let showAboutSidebar = () => {
-//    AboutSidebarToggle = true;
-//    console.log(AboutSidebarToggle)
-// }
-// 
-// let hideAboutSidebar = () => {
-//    AboutSidebarToggle = false;
-//    console.log(AboutSidebarToggle)
-// }
-
-let sidebar = () => (
-   <div className={style.sidebar}>
-      <ThinSidebar />
-      <AboutSidebar className={style.about_sidebar} />
-
+export default () => (
+   <div className={styles.sidebar}>
+      <Link to="/">
+         <h1 className={styles.vertical_text}>Cécile&nbsp;Lebleu</h1>
+      </Link>
+      <div className={styles.menu}>
+         <Link
+            to="about"
+            className={styles.vertical_text}
+            activeClassName = {styles.active}
+            partiallyActive={true}
+         >about</Link>
+         <Link
+            to="work"
+            className={styles.vertical_text}
+            activeClassName = {styles.active}
+            partiallyActive={true}
+         >work</Link>
+         <Link
+            to="blog"
+            className={styles.vertical_text}
+            activeClassName = {styles.active}
+            partiallyActive={true}
+         >blog</Link>
+         <Link
+            to="playground"
+            className={styles.vertical_text}
+            activeClassName = {styles.active}
+            partiallyActive={true}
+         >play</Link>
+      </div>
+      <div className={styles.icons}>
+         <IconContainer
+            link="https://dev.to/cecilelebleu"
+            name={<DevToLogo />}
+         />
+         <IconContainer
+            className={styles.icon_container}
+            link="https://dribbble.com/CzlLebleu"
+            name={<DribbbleLogo />}
+         />
+         <IconContainer
+            className={styles.icon_container}
+            link="https://github.com/Cecile-Lebleu"
+            name={<GithubLogo />}
+         />
+         <IconContainer
+            className={styles.icon_container}
+            link="https://pinterest.com/czllebleu/"
+            name={<PinterestLogo />}
+         />
+         <IconContainer
+            className={styles.icon_container}
+            link="https://twitter.com/CzlLebleu/"
+            name={<TwitterLogo />}
+         />
+         <IconContainer
+            className={styles.icon_container}
+            link="mailto:hello@cecilelebleu.com?Subject=Hello!"
+            name={<MailLogo />}
+         />
+         <IconContainer className={styles.icon_container} name={<Fleuron />} />
+      </div>
    </div>
 )
 
-
-export default sidebar;
- // onMouseOver={showAboutSidebar} onMouseOut={hideAboutSidebar}
+// <IconContainer
+//    className={sidebarStyles.icon_container}
+//    name={<ColorModeSwitch />}
+// />
