61451f326bd91103a0cd1d0af0cd44c20134014c
Czl Lebleu <czl.lebleu@gmail.com>
2020-03-11 23:52:46 -0600

    create blog post component

---
+++ src/components/blog-post.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "./layout"
+import TextContent from "../components/TextContent"
+import styles from "./blog-post.module.scss"
+
+export default ({ data }) => {
+   const post = data.markdownRemark
+   return (
+      <Layout>
+         <TextContent>
+            <h1>{post.frontmatter.title}</h1>
+            <p className={styles.mono}>Posted on {post.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+         </TextContent>
+      </Layout>
+   )
+}
+ // TODO: Add categories/tags here
+export const query = graphql`
+   query($slug: String!) {
+      markdownRemark(fields: { slug: { eq: $slug } }) {
+         html
+         frontmatter {
+            title
+            description
+            date(formatString: "MMMM DD, YYYY")
+         }
+      }
+   }
+`
