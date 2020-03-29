260deaaac4c4fd9608fd44f31b2a25c8ee1914ac
Cécile Lebleu <czl.lebleu@gmail.com>
2020-03-28 18:42:06 -0600

    Global Styles page

---
+++ src/pages/styles.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+import TextContent from "../components/TextContent"
+
+export default ({ children }) => (
+   <Layout>
+      <TextContent>
+         <h1>Heading 1</h1>
+         <h2>Heading 2</h2>
+         <h3>Heading 3</h3>
+         <h4>Heading 4</h4>
+         <h5>Heading 5</h5>
+         <h6>Heading 6</h6>
+         <p>abcdefghijklmnopqrstuvwxyz|abcdefghijklmnopqrstuvwxyz|abcdefghijklmnopqrstuvwxyz|abcdefghijklmnopqrstuvwxyz</p>
+         <p>Do anim id reprehenderit minim eiusmod nostrud enim. Commodo pariatur enim sunt enim Lorem quis sint elit duis anim ex. Ut Lorem deserunt amet in elit exercitation amet nostrud nulla proident esse laboris adipisicing voluptate. Proident mollit labore ex veniam duis aute. Lorem nisi duis aute laborum in dolor. Laboris esse cupidatat velit sit consectetur ut anim. Labore velit consectetur sunt culpa occaecat.</p>
+      </TextContent>
+   </Layout>
+)
