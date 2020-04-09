24d6a4f07489e6c167ebf84dbcbbf21b4aa6ff2b
Cécile Lebleu <czl.lebleu@gmail.com>
2020-04-09 14:16:43 -0600

    Replace dark syntax theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,21 +19,13 @@ module.exports = {
                   options: {
                      theme: {
                         default: 'Solarized Light',
-                        dark: 'Dark+ (default dark)'
+                        dark: 'Monokai'
                      },
                      wrapperClassName: 'code',
                      injectStyles: false,
                      replaceColor: oldColor => ({
                         '#FDF6E3': 'var(--highlightCode)'
                      })[oldColor.toLowerCase()] || oldColor
-                     // replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-                     // getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
-                     //    content,              //   - the string content of the line
-                     //    index,                //   - the zero-based index of the line within the code fence
-                     //    language,             //   - the language specified for the code fence
-                     //    meta                  //   - any options set on the code fence alongside the language (more on this later)
-                     // }) => '',
-                     // logLevel: 'warn'       // Set to 'info' to debug if something looks wrong
                   }
                }
             ]
