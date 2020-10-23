601da87b8cbf691fbfe1d41b8c75dca809666e8d
Cécile Lebleu <czl.lebleu@gmail.com>
2020-10-23 15:26:57 -0600

    Re-start homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 import styles from "./index.module.scss"
 
 import Layout from "../components/layout"
-import About from "../components/about"
+import Card from '../components/card'
 
 export default () => (
    <Layout>
@@ -14,16 +14,30 @@ export default () => (
          <title>Cécile Lebleu</title>
          <meta name="Description" content="Portfolio website of designer & developer Cécile Lebleu" />
       </Helmet>
-      <div className={styles.intro}>
-         <div className={styles.container}>
-            <div className={styles.title}>
-               <span className={styles.fleuron}>❧</span>
-               <h2><span>Front</span><br /><span>End & UI</span><br /><span>Developer</span></h2>
-            </div>
-         </div>
-         <div>
-            <About />
-         </div>
-      </div>
+		<div className={styles.area1}>
+			<Card backgroundColor="#ffffff">
+				<div className={styles.deco}>
+					{/* <p>a</p> */}
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+				<h2>
+					<span>Front</span>
+					<br />
+					<span>End & UI</span>
+					<br />
+					<span>Developer</span>
+				</h2>
+			</Card>
+			<Card backgroundColor="#272727">
+				<p>Nice to meet you! I’m <span>Cécile Lebleu</span>, brand designer turned front-end developer.</p>
+				<p>I build interactive websites, brand identities, and design systems. I also like to make great food.</p>
+				<Link to="/about/" >
+					<button style={{width: "100%"}}>About</button>
+				</Link>
+			</Card>
+		</div>
    </Layout>
 )
